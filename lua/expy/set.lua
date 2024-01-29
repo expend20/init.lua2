f35c0984c8c479b7ffdb26de501b02ed9225148b
expy <expy@>
2024-01-29 08:55:27 -0500

    wsl disabled

--- lua/expy/set.lua
+++ lua/expy/set.lua
@@ -1,12 +1,10 @@
 -- vim.opt.guicursor = ""
 -- here is how check host os:
-if vim.fn.has("win32") then
-    print("windows detected")
-elseif vim.fn.has("wsl") then
-    print("wsl detected")
-elseif vim.fn.has("unix") then
-    print("unix detected")
-end
+-- if vim.fn.has("win32") then
+--     print("windows detected")
+-- elseif vim.fn.has("unix") then
+--     print("unix detected")
+-- end
 
 vim.opt.list = true
 vim.opt.ignorecase = true
@@ -41,12 +39,17 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
--- enabling clipboard for wsl
+-- enabling clipboard for wsl UPD: currently not neeeded, fixed?
 -- https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-if vim.fn.has("wsl") or vim.fn.has("win32") then
-  vim.opt.clipboard = "unnamedplus"
-end
+-- if vim.fn.has("wsl") or vim.fn.has("win32") then
+--   vim.opt.clipboard = "unnamedplus"
+-- end
 
+vim.api.nvim_set_keymap("n", "<leader>t2", ":set tabstop=2<CR>:set shiftwidth=2<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>t4", ":set tabstop=4<CR>:set shiftwidth=4<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tt", ":set expandtab!<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>td", ":set ff=dos<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tu", ":set ff=unix<CR>", {noremap = true, silent = true})
 
 
 
